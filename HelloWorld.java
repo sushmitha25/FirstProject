fba5deb332f38afdc2020a167adc50c4e0bdf370
sushmitha25 <44090092+sushmitha25@users.noreply.github.com>
2018-10-17 17:50:14 +0530

    Update HElloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
+package com.srccodes.example;
+
 public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+
+	}
+
 }
